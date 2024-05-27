dee84709680f96e6e7adb642b3d888312b806014
Nirjoy Debnath <nirjoydebnath61@gmail.com>
2024-05-27 16:45:59 +0600

    Insert user implemented

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import db from './databaseConnection/db'
 import userRouter from './routes/userRoute'
+import {userType} from './interfaces/user'
+import { Next } from 'mysql2/typings/mysql/lib/parsers/typeCast';
 
 dotenv.config();
 
@@ -12,12 +14,6 @@ const port = process.env.port || 3000;
 app.use(express.json());
 app.use('/api/users', userRouter.router)
 
-interface userType {
-    Id:number,
-    UserName: string,
-    Email: string,
-    Password: string,
-}
 
 app.get('/', async(req: Request, res: Response) =>{
     try{
@@ -26,6 +22,10 @@ app.get('/', async(req: Request, res: Response) =>{
     } catch (err){
         res.status(500).json({error: err})
     }
+})
+
+app.use((err:Error, req:Request, res: Response, next: NextFunction) =>{
+    res.status(500).send(err.message)
 })
 
 app.listen(port, () =>{
