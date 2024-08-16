2952e96d202257b6d531fcf277d11428b21d9c83
Nirjoy Debnath <nirjoydebnath61@gmail.com>
2024-08-16 10:41:32 +0600

    Story per page increased

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,12 +1,12 @@
 export const UserPerPage: number = 3;
-export const StoryPerPage: number = 6;
+export const StoryPerPage: number = 12;
 export const UserNameMinLength: number = 1;
 export const UserNameMaxLength: number = 20;
 export const NameMinLength: number = 1;
 export const NameMaxLength: number = 30;
 export const NamePattern: RegExp = /^[a-zA-Z.\s]*$/;
 export const PasswordPattern: RegExp =
-  /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/;
+  /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d\W_]{8,}$/;
 export const StoryTitleMinLength: number = 1;
 export const StoryTitleMaxLength: number = 100;
 export const StoryDescriptionMinLength: number = 10;
